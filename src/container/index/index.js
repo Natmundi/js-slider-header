db5cb406edc2bc63ad5641819593740bbe05d3ae
Natmundi <n.v.cherevata@outlook.com>
2023-09-29 17:08:09 +0200

    slide finish

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -1,3 +1,88 @@
-import '../../script/test'
+class Slider {
+  // це те що є в контенті
+  static #content = null
 
-console.log('container')
+  // buttons
+  static #left = null
+  static #right = null
+
+  // це поточна картинка яка показується в слайдері
+  static #count = 1
+  // це число максимальної кількості картинок(4картинки)
+  static #max = null
+
+  // підключаємо всі потрібні елементи
+  static init = () => {
+    this.#content = document.querySelector(
+      '.slider__content',
+    )
+    this.#left = document.querySelector(
+      '.slider__button--left',
+    )
+    this.#right = document.querySelector(
+      '.slider__button--right',
+    )
+
+    this.#max = this.#content.childElementCount
+
+    // функції які перевертають слайди
+    this.#left.onclick = () => this.#slide('left')
+    this.#right.onclick = () => this.#slide('right')
+  }
+
+  static #slide = (side) => {
+    const offsetWidth = this.#content.offsetWidth
+    const scrollLeft = this.#content.scrollLeft
+    const scrollWidth = this.#content.scrollWidth
+
+    let scroll = 0
+
+    if (side === 'left') {
+      if (this.#count === 1 || scrollLeft === 0) {
+        this.#count = this.#max
+        scroll = (this.#count - 1) * offsetWidth
+      } else {
+        this.#count -= 1
+        scroll = (this.#count - 1) * offsetWidth
+      }
+    } else if (side === 'right') {
+      if (
+        this.#count === this.#max ||
+        scrollLeft === scrollWidth - offsetWidth
+      ) {
+        this.#count = 1
+        scroll = 0
+      } else {
+        this.#count += 1
+        scroll = (this.#count - 1) * offsetWidth
+      }
+    }
+
+    // if (side === 'left') {
+    //   if (this.#count === 1) {
+    //     this.#count = this.#max
+    //     scroll = (this.#count - 1) * offsetWidth
+    //   } else {
+    //     this.#count -= 1
+    //     scroll = (this.#count - 1) * offsetWidth
+    //   }
+    // }
+
+    // if (side === 'right') {
+    //   if (this.#count === this.#max) {
+    //     this.#count = 1
+    //     scroll = 0
+    //   } else {
+    //     this.#count += 1
+    //     scroll = (this.#count - 1) * offsetWidth
+    //   }
+    // }
+
+    this.#content.scrollTo({
+      top: 0,
+      left: scroll,
+      behavior: 'smooth',
+    })
+  }
+}
+Slider.init()

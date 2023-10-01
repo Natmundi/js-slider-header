61e9ea50fc0d39b10286b8e98dae23a7e57cdb90
Natmundi <n.v.cherevata@outlook.com>
2023-10-01 19:01:31 +0200

    finish js-slider-header

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -22,7 +22,7 @@ class Slider {
     this.#right = document.querySelector(
       '.slider__button--right',
     )
-
+    // зф рахунок цьго ми знаємо скільки в нас є картинок
     this.#max = this.#content.childElementCount
 
     // функції які перевертають слайди
@@ -86,3 +86,49 @@ class Slider {
   }
 }
 Slider.init()
+
+class Header {
+  static #height = null
+  static #wrapper = null
+  static #button = null
+
+  static #isOpen = false
+
+  static init() {
+    // робимо змінну для висоті розкритого меню
+    this.#height = document.querySelector(
+      '.header__bottom',
+    ).offsetHeight
+    // Робимо змінну для меню розкладувального
+    this.#wrapper = document.querySelector(
+      '.header__wrapper',
+    )
+    this.#button = document.querySelector('.header__button')
+
+    this.#button.onclick = this.#toggle
+  }
+
+  // робимо метод заміни розкритого меню на закрите і навпаки
+  static #toggle = () => {
+    // перевіряємо чи відкрите вікно
+    if (this.#isOpen) {
+      this.#button.classList.replace(
+        'header__button--close',
+        'header__button--open',
+      )
+      // якщо хочу закрити вікно то просто ставлю 0
+      this.#wrapper.style.height = 0
+    } else {
+      this.#button.classList.replace(
+        'header__button--open',
+        'header__button--close',
+      )
+      // якщо хочу відкрити вікно то ставлю йому розмір з scss
+      this.#wrapper.style.height = `${this.#height}px`
+    }
+
+    this.#isOpen = !this.#isOpen
+  }
+}
+
+Header.init()
